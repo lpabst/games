caff2f9f936a88b4d60a4cdab56342ef97999273
lorenpabst@gmail.com <lorenpabst@gmail.com>
2018-06-13 16:49:03 -0600

    added update and render functions

--- src/components/Art/canvasJS/sandPile.js
+++ src/components/Art/canvasJS/sandPile.js
@@ -6,12 +6,13 @@ var sandPile = {
         var animationFrame = 0;
 
         var sandArray = [];
-        for (var i = 0; i < 201; i++){
+        for (var i = 0; i <= 200; i++){
             sandArray[i] = [];
-            for (var j = 0; j < 201; j++){
+            for (var j = 0; j <= 200; j++){
                 sandArray[i][j] = 0;
             }
         }
+        sandArray[100][100] = 400;
 
         artClass.data = { canvas, context, animationFrame, sandArray };
 
@@ -29,11 +30,43 @@ var sandPile = {
     },
 
     update: function(artClass){
+        // Copy sandArray
+        let { sandArray } = artClass.data;
+        let nextFrameSandArray = JSON.parse(JSON.stringify(sandArray));
+        
+        // Piles that are too large overflow onto their neighbors here
+        for (var i = 0; i <= 200; i++){
+            for (var j = 0; j <= 200; j++) {
+                if (sandArray[i][j] >= 4){
+                    nextFrameSandArray[i][j] -= 4;
+                    nextFrameSandArray[i][j+1] ++;
+                    nextFrameSandArray[i][j-1] ++;
+                    nextFrameSandArray[i+1][j] ++;
+                    nextFrameSandArray[i-1][j] ++;
+                }
+            }
+        }
 
+        artClass.data.sandArray = JSON.parse(JSON.stringify(nextFrameSandArray));
     },
 
     render: function(artClass){
+        let { canvas, context, sandArray } = artClass.data
 
+        // Black background
+        context.fillStyle = '#000000';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        // index maps to the number of sand particles on any pixel
+        var colorMapping = ['#000', '#551c00', '#803815', '#d48d6a', '#ffc6aa'];
+
+        for (var i = 0; i <= 200; i++){
+            for (var j = 0; j <= 200; j++) {
+                var numSandParticles = sandArray[i][j];
+                context.fillStyle = colorMapping[numSandParticles] ? colorMapping[numSandParticles] : '#ffc6aa';
+                context.fillRect(i*3, j*3, 3, 3);
+            }
+        }
     },
 }
 
