3c6622bd767bcc610aff7919281477dde25daadd
lorenpabst@gmail.com <lorenpabst@gmail.com>
2018-06-18 12:36:10 -0600

    added link to sandpile article

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -39,8 +39,8 @@ class Art extends Component {
         <div className="gameWrapper" >
 
           <div className='artWrapper'>
-            <h1>Sandpiles</h1>
-            <p>Visualization of looking at the top of a sand pile as sand is dropped onto the top of it. As too much sand accumulates in one area, it topples onto its neighbors. By default the sand is dropped in the middle of the black background, but you can also click anywhere on the canvas to drop some sand there as well. Click a bunch of places and see what happens! The world is your oyster</p>
+            <h1>Sand Piles</h1>
+            <p>Visualization of looking at the top of a sand pile as sand is dropped onto the top of it. As too much sand accumulates in one area, it topples onto its neighbors. By default the sand is dropped in the middle of the black background, but you can also click anywhere on the canvas to drop some sand there as well. Click a bunch of places and see what happens! The world is your oyster. Learn more <span><a target='_blank' href='https://en.wikipedia.org/wiki/Abelian_sandpile_model'>here</a></span>.</p>
             <div className='canvasWrapper'>
               <canvas width='600' height='600' id='sandpileCanvas' onMouseDown={(e) => this.clickToDropSand(e)} ></canvas>
             </div>
@@ -49,7 +49,7 @@ class Art extends Component {
           <div className='artWrapper'>
             
             <h1>Game of Life</h1>
-            <p>Visualization of the game of life, a cellular automaton devised by the British mathematician John Horton Conway in 1970. Each cell is either alive or dead, and is updated each frame depending on how many alive neighbors it has. I've also added a button so you can toggle a 'fading' effect so you can watch the cell slowly fade as it "dies". Learn more <span><a href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'>here</a></span>.</p>
+            <p>Visualization of the game of life, a cellular automaton devised by the British mathematician John Horton Conway in 1970. Each cell is either alive or dead, and is updated each frame depending on how many alive neighbors it has. I've also added a button so you can toggle a 'fading' effect so you can watch the cell slowly fade as it "dies". Learn more <span><a target='_blank' href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'>here</a></span>.</p>
             
             <center>
               <button onClick={() => this.toggleGameOfLifeFadingEffect()} >Toggle Fading Effect</button>
