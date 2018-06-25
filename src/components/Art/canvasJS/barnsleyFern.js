047d750cd031b87e9182eb15ba078579955a578a
lorenpabst@gmail.com <lorenpabst@gmail.com>
2018-06-25 11:29:05 -0600

    added code to add new points to the canvas for the fern

--- src/components/Art/canvasJS/barnsleyFern.js
+++ src/components/Art/canvasJS/barnsleyFern.js
@@ -1,17 +1,20 @@
-var barnsleyFern = {
-    init: function(artClass){
+const barnsleyFern = {
+    init: function (artClass) {
 
-        var canvas = document.getElementById('fernCanvas');
-        var context = canvas.getContext('2d');
-        var animationFrame = 0;
+        let canvas = document.getElementById('fernCanvas');
+        let context = canvas.getContext('2d');
+        let animationFrame = 0;
+
+        let points = [[300, 590]];
+        let scale = 2;
 
         artClass.fern = {};
-        artClass.fern.data = { canvas, context, animationFrame };
+        artClass.fern.data = { canvas, context, animationFrame, points, scale };
 
         barnsleyFern.run(artClass);
     },
 
-    run: function(artClass){
+    run: function (artClass) {
         function loop() {
             barnsleyFern.update(artClass);
             barnsleyFern.render(artClass);
@@ -21,39 +24,43 @@ var barnsleyFern = {
         loop();
     },
 
-    update: function(artClass){
-       
+    // x = -2.1820 - 2.6558 && y = 0 - 9.9983
+    update: function (artClass) {
+        let { points } = artClass.fern.data;
+        let x = points[points.length - 1][0];
+        let y = points[points.length - 1][1];
+        let nextX, nextY;
+        let rand = Math.random();
+
+        if (rand < 0.01){
+            nextX = 0;
+            nextY = 0.16 * y;
+        }else if (rand < 0.86){
+            nextX = 0.85 * x + 0.04 * y;
+            nextY = -0.04 * x + 0.85 * y + 1.6
+        }else if (rand < 0.93){
+            nextX = 0.2 * x - 0.26 * y;
+            nextY = 0.23 * x + 0.22 * y + 1.6;
+        }else{
+            nextX = -0.15 * x + 0.28 * y;
+            nextY = 0.26 * x + 0.24 * y + 0.44;
+        }
+
+        artClass.fern.data.points.push([nextX, nextY])
     },
 
-    render: function(artClass){
-        // let { canvas, context, resolution, fadingEffect, rows, cols, grid } = artClass.barnsleyFern.data;
+    render: function (artClass) {
+        let { canvas, context, points, scale } = artClass.fern.data;
 
-        // // Black background
-        // context.fillStyle = '#000000';
-        // context.fillRect(0, 0, canvas.width, canvas.height);
+        // Black background
+        context.fillStyle = '#000000';
+        context.fillRect(0, 0, canvas.width, canvas.height);
 
-        // let colorMapping = ['#222', '#333', '#444', '#555', '#666', '#777', '#888', '#999', '#aaa', '#bbb', '#ccc', '#ddd', '#eee'];
-
-        // // Draw the grid
-        // for (let i = 0; i < rows; i++){
-        //     for (let j = 0; j < cols; j++){
-
-        //         context.fillStyle = '#111';
-
-        //         if (grid[i][j].alive) { 
-        //             context.fillStyle = '#fff'; 
-        //         }else if (fadingEffect){
-        //             for (let k = grid[i][j].history.length-1; k >= 0; k--){
-        //                 if (grid[i][j].history[k]){
-        //                     context.fillStyle = colorMapping[k];
-        //                     break;
-        //                 }
-        //             }
-        //         }
-
-        //         context.fillRect(i*resolution, j*resolution, resolution-1, resolution-1);
-        //     }
-        // }
+        // Draw the points
+        context.fillStyle = '#fff';
+        points.forEach(location => {
+            context.fillRect(location[0], location[1], scale, scale);
+        })
     },
 }
 
